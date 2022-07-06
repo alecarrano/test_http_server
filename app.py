fef9ef74bc2d1c2ceb30c3326a973802afd8c1b1
Alessandro Carrano <alessandro.carrano@gmail.com>
2022-07-06 15:52:43 +0200

    Update app.py

--- app.py
+++ app.py
@@ -6,6 +6,7 @@ import logging
 import time
 import sys
 import os
+import psycopg2
 
 PORT = 8000
 
@@ -17,7 +18,11 @@ class GetHandler(
         logging.error(self.headers)
         logging.error(os.environ.get('PRINT_VAR', '/home/username/'))
         SimpleHTTPServer.SimpleHTTPRequestHandler.do_GET(self)
-
+        conn = psycopg2.connect(
+                database=os.environ.get('DB_DBNAME', 'f'), user=os.environ.get('DB_USER', 'f'), password=os.environ.get('DB_PASSWORD', 'f'), host=os.environ.get('DB_HOST', 'f'), port= os.environ.get('DB_PORT', 'f')
+                )
+        logging.error('connected')
+        
     def do_POST(self):
         logging.error(self.headers)
         logging.error(os.environ.get('PRINT_VAR', '/home/username/'))
